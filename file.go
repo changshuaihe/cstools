dde2dea81f2e8f3a1f4a77ce40b44d2b4ebf405b
hechangshuai <hechangshuai@autohome.com.cn>
2020-12-07 14:26:14 +0800

    增加写文件和判断文件存在

--- file.go
+++ file.go
@@ -1,7 +1,9 @@
 package cstools
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func ReadFile(fileName string) (string, error) {
@@ -11,4 +13,28 @@ func ReadFile(fileName string) (string, error) {
 	}
 
 	return string(b), nil
+}
+
+func IsFileExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+func appendToFile(fileName string, content string) error {
+	// 以只写的模式，打开文件
+	f, err := os.OpenFile(fileName, os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("cacheFileList.yml file create failed. err: " + err.Error())
+
+	} else {
+		// 查找文件末尾的偏移量
+		n, _ := f.Seek(0, os.SEEK_END)
+		// 从末尾的偏移量开始写入内容
+		_, err = f.WriteAt([]byte(content), n)
+	}
+	defer f.Close()
+	return err
 }
